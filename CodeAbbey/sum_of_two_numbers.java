8fa528f705dd74aab215f6b2781a5f81b443f1a4
Subham222006 <subhamnandi222006@gmail.com>
2025-12-24 00:50:03 +0530

    Java

---
+++ CodeAbbey/sum_of_two_numbers.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class sum_of_two_numbers{
+    public static void main(String[] arg){
+        Scanner in = new Scanner(System.in);
+        int n1=in.nextInt();        
+        int n2=in.nextInt();
+        int s=n1+n2;
+        System.out.println(s);
+        
+    }
+}
